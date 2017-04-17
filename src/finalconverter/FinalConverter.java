306e988dc0df9b31859789bc9465f7f5d4818e09
istipanya <akosimeilingtan@gmail.com>
2017-04-17 16:52:31 +0800

    Initial Commit

---
+++ src/finalconverter/FinalConverter.java
@@ -0,0 +1,35 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+package finalconverter;
+
+import java.util.Scanner;
+
+/**
+ *
+ * @author IstipanyaGrasya
+ */
+public class FinalConverter {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        String baseTemp;
+        double temp;
+        double answer;
+
+        Scanner input = new Scanner(System.in);
+
+        System.out.println("Choose Base Temperature: ");
+        System.out.println("A: Celcius");
+        System.out.println("B: Fahrenhiet");
+
+        baseTemp = input.next();
+    }
+    
+}
